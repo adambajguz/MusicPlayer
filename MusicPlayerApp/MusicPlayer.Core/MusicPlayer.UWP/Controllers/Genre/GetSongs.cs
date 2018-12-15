34aee6f93153046c4cfc3eebdf0453d1761311f9
Unknown <vaporbae@gmail.com>
2018-12-16 00:42:47 +0100

    Genre controller methods

---
+++ MusicPlayerApp/MusicPlayer.Core/MusicPlayer.UWP/Controllers/Genre/GetSongs.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicPlayer.Core.CQRS;
+using MusicPlayer.Core.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.UWP.Controllers.Genre
+{
+    public class GetSongs
+    {
+        public class Query : IQuery
+        {
+            public int ID;
+        }
+        public class HandlerList : IQueryHandler<Query, List<Song.Result>>
+        {
+            private IUnitOfWork _uow;
+            public HandlerList(IUnitOfWork uow)
+            {
+                _uow = uow;
+            }
+
+            public async Task<List<Song.Result>> Handle(Query query)
+            {
+                var result = await _uow.SongRepository.Query().Where(x => x.GenreId==query.ID).Select(x => new Song.Result(x)).ToListAsync();
+
+                return result;
+            }
+
+        }
+    }
+}
---
+++ MusicPlayerApp/MusicPlayer.Core/MusicPlayer.UWP/Controllers/Genre/SearchGenres.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicPlayer.Core.CQRS;
+using MusicPlayer.Core.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.UWP.Controllers.Genre
+{
+    public class SearchGenres
+    {
+        public class Query : IQuery
+        {
+            public string Name;
+        }
+        public class HandlerList : IQueryHandler<Query, List<Result>>
+        {
+            private IUnitOfWork _uow;
+            public HandlerList(IUnitOfWork uow)
+            {
+                _uow = uow;
+            }
+
+            public async Task<List<Result>> Handle(Query query)
+            {
+                var result = await _uow.GenreRepository.Query().Where(x => x.Name.ToUpper().Contains(query.Name.ToUpper())).Select(x => new Result(x)).OrderBy(y => y.Name).ToListAsync();
+
+                return result;
+            }
+
+        }
+    }
+}
