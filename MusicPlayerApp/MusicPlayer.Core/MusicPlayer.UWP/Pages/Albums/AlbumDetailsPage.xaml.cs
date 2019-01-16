93608da2f1b4070f8ec992dc9d9ef810bcd554c9
Adam Bajguz <adam.bajguz@gmail.com>
2019-01-16 20:31:26 +0100

    Album improvements

--- MusicPlayerApp/MusicPlayer.Core/MusicPlayer.UWP/Pages/Albums/AlbumDetailsPage.xaml.cs
+++ MusicPlayerApp/MusicPlayer.Core/MusicPlayer.UWP/Pages/Albums/AlbumDetailsPage.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Animation;
@@ -77,8 +78,104 @@ namespace MusicPlayer.UWP.Pages.Albums
             var x = e.NewItems;
         }
 
+        private async void MenuFlyoutItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is MenuFlyoutItem selectedItem)
+            {
+                List<Controllers.Song.Result> temp;
 
 
+                string sortOption = selectedItem.Tag.ToString();
+                switch (sortOption)
+                {
+                    case "az":
+                        temp = await songController.GetAll();
+                        break;
+
+                    case "za":
+                        temp = await songController.GetAllDescending();
+                        break;
+
+                    case "len_az":
+                        temp = await songController.GetAllByLength();
+                        break;
+
+                    case "len_za":
+                        temp = await songController.GetAllByLengthDescending();
+                        break;
+
+                    case "play_az":
+                        temp = await songController.GetAllByPlayTimes();
+                        break;
+
+                    case "play_za":
+                        temp = await songController.GetAllByPlayTimesDescending();
+                        break;
+
+                    case "score_az":
+                        temp = await songController.GetAllByScore();
+                        break;
+
+                    case "score_za":
+                        temp = await songController.GetAllByScoreDescending();
+                        break;
+
+                    case "date_az":
+                        temp = await songController.GetAllByDate();
+                        break;
+
+                    case "date_za":
+                        temp = await songController.GetAllByDateDescending();
+                        break;
+
+                    default:
+                        return;
+                }
+
+                List<Controllers.Song.Result> albumSongs = await albumController.GetSongs(elementID.Value);
+                List<int> albumSongsIds = new List<int>();
+
+                foreach (Controllers.Song.Result item in albumSongs)
+                    albumSongsIds.Add(item.Id);
+
+                List<Controllers.Song.Result> toDisplay = new List<Controllers.Song.Result>();
+                foreach (Controllers.Song.Result item in temp)
+                    if (albumSongsIds.Contains(item.Id))
+                        toDisplay.Add(item);
+
+                songs.Clear();
+                songs.AddRange(toDisplay);
+            }
+        }
+
+
+
+        private void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is AppBarButton selectedItem)
+            {
+                switch (selectedItem.Tag.ToString())
+                {
+                    case "Add":
+                        mainPage.NavView_Navigate(MainPage.ArtistAddTag, new EntranceNavigationTransitionInfo(), null);
+
+                        break;
+
+                    case "Remove":
+                        DisplayDeleteListDialog(SongsListView.SelectedItems);
+
+                        break;
+
+                }
+            }
+        }
+        private void GenresListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (SongsListView.SelectedItems.Count > 0)
+                DeleteSelected.IsEnabled = true;
+            else
+                DeleteSelected.IsEnabled = false;
+        }
 
         private async void ListItemBarButton_Click(object sender, RoutedEventArgs e)
         {
@@ -92,8 +189,22 @@ namespace MusicPlayer.UWP.Pages.Albums
 
                     switch (selectedItem.Name.ToString())
                     {
+                        case "IPlay":
+
+                            break;
+
                         case "IDetails":
                             mainPage.NavView_Navigate(MainPage.SongDetailsTag, new EntranceNavigationTransitionInfo(), selectedSong.Id);
+
+                            break;
+
+                        case "IEdit":
+                            mainPage.NavView_Navigate(MainPage.SongEditTag, new EntranceNavigationTransitionInfo(), selectedSong.Id);
+
+                            break;
+
+                        case "IRemove":
+                            DisplayDeleteSingleDialog(selectedSong);
 
                             break;
                     }
@@ -102,5 +213,65 @@ namespace MusicPlayer.UWP.Pages.Albums
 
             }
         }
+
+
+        private async void DisplayDeleteSingleDialog(Controllers.Song.Result bandToDelete)
+        {
+            ContentDialog deleteFileDialog = new ContentDialog
+            {
+                Title = "Delete '" + bandToDelete.Title + "' permanently?",
+                Content = "If you delete this band, you won't be able to recover it. Do you want to delete it?",
+                PrimaryButtonText = "Delete",
+                CloseButtonText = "Cancel"
+            };
+
+            ContentDialogResult result = await deleteFileDialog.ShowAsync();
+
+
+            if (result == ContentDialogResult.Primary)
+            {
+                // Delete
+
+                await songController.Delete(bandToDelete.Id);
+
+                songs.Clear();
+                songs.AddRange(await albumController.GetSongs(elementID.Value));
+            }
+            else
+            {
+                // The user clicked the CLoseButton, pressed ESC, Gamepad B, or the system back button.
+                // Do nothing.
+            }
+        }
+
+        private async void DisplayDeleteListDialog(IList<object> genresToDelete)
+        {
+            ContentDialog deleteFileDialog = new ContentDialog
+            {
+                Title = "Delete selected songs permanently?",
+                Content = "If you delete these songs, you won't be able to recover them. Do you want to delete them?",
+                PrimaryButtonText = "Delete",
+                CloseButtonText = "Cancel"
+            };
+
+            ContentDialogResult result = await deleteFileDialog.ShowAsync();
+
+
+            if (result == ContentDialogResult.Primary)
+            {
+                // Delete
+                foreach (Controllers.Song.Result song in genresToDelete)
+                    await songController.Delete(song.Id);
+
+                songs.Clear();
+                songs.AddRange(await albumController.GetSongs(elementID.Value));
+            }
+            else
+            {
+                // The user clicked the CLoseButton, pressed ESC, Gamepad B, or the system back button.
+                // Do nothing.
+            }
+        }
     }
+
 }
