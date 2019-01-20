c40c1b4e24ee7ed3ad8ed3e103b8f5e16ed2aca7
Unknown <vaporbae@gmail.com>
2019-01-20 11:57:18 +0100

    Song - parametless constructor added

--- MusicPlayerApp/MusicPlayer.Core/MusicPlayer.UWP/Controllers/Song/Result.cs
+++ MusicPlayerApp/MusicPlayer.Core/MusicPlayer.UWP/Controllers/Song/Result.cs
@@ -1,4 +1,6 @@
-﻿namespace MusicPlayer.UWP.Controllers.Song
+﻿using System;
+
+namespace MusicPlayer.UWP.Controllers.Song
 {
     public class Result : Core.Entities.Song
     {
@@ -15,6 +17,21 @@
             bitrate = x.bitrate;
             DBCreationDate = x.DBCreationDate;
             PlayTimes = x.PlayTimes;
+        }
+
+        private Result()
+        {
+            Id = 100000;
+            ImageId = 100000;
+            GenreId = 100000;
+            Score = 3;
+            Title = "tytul";
+            CreationDate = DateTime.UtcNow;
+            FilePath = "/path";
+            Length = TimeSpan.FromSeconds(120);
+            bitrate = 120;
+            DBCreationDate = DateTime.UtcNow;
+            PlayTimes = 0;
         }
 
         public string LengthText
